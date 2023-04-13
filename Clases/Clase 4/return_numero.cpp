71a20d085fa37de953ba17deac7108fd185a3a6c
Raztor <contacto@webizy.cl>
2023-04-13 10:17:21 -0400

    Commit pendiente clase 4

---
+++ Clases/Clase 4/return_numero.cpp
@@ -0,0 +1,13 @@
+//
+// Created by benjamin on 21-03-23.
+//
+
+#include <stdio.h>
+#include <math.h>
+int main(){
+    int entero=0;
+    printf("Ingrese un número entero");
+    scanf("%i", &entero);
+    printf("%i",entero);
+    return 0;
+}
---
+++ Clases/Clase 4/saludo.cpp
@@ -0,0 +1,13 @@
+//
+// Created by benjamin on 21-03-23.
+//
+#include <stdio.h>
+
+void hello(){
+    printf("Hola");
+}
+
+int main(){
+    hello();
+    return 0 ;
+}
