95c8d263336c81fca014f3498af63117acc03c02
Raztor <contacto@webizy.cl>
2023-04-13 10:17:52 -0400

    Commit inicial Tarea 1

---
+++ tareas/tarea-1/src/main.c
@@ -0,0 +1,5 @@
+//
+// Created by benjamin on 13-04-23.
+//
+
+#include "main.h"
---
+++ tareas/tarea-1/src/main.h
@@ -0,0 +1,8 @@
+//
+// Created by benjamin on 13-04-23.
+//
+
+#ifndef EDA_MAIN_H
+#define EDA_MAIN_H
+
+#endif //EDA_MAIN_H
